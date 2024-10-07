3907c21b141659ef187962240f5af9eb5f340f7e
Malkium <eliott.hall.pro@gmail.com>
2024-10-07 15:22:00 +0200

    Add files via upload

---
+++ factoriel.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Recursive function to calculate factorial
+factorial() {
+    local n=$1
+    if (( n == 0 )); then
+        echo 1
+    else
+        echo $(( n * $(factorial $((n - 1))) ))
+    fi
+}
+
+while true
+do
+	let i=i+1000
+	result=$(factorial $i)
+	echo -e "$i"
+	echo -e "$result"
+done
